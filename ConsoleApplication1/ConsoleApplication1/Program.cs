c0a46e8343e0f053e62263f495eeb56c87aabbe1
Parocep <pablogpecora@gmail.com>
2025-05-15 08:27:24 -0300

    Add files via upload

---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, fahrenheit;
+            Console.Write("Ingrese una temperatura en celsius: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            fahrenheit = num1 * 1.8 + 32;
+            Console.WriteLine("La temperatura es de " + fahrenheit + " fahrenheit");
+            Console.ReadKey();
+        }
+    }
+}
